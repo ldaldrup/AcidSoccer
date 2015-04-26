24ceb585611b58f3dd618b4ffbd50d43afd443fd
moe-ped <m.berning@web.de>
2015-04-26 17:17:24 +0200

    Rules 1 - added scoreboard - added ball object - added goal object

---
+++ Assets/Goal.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	// reference to goals object
+	public static Rules rules;
+
+	public void setRulesRef (Rules rulesRef)
+	{
+		rules = rulesRef;
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Ball")
+		{
+			//Ball ball = GetComponent<Ball>();
+			// test
+			if (rules == null)
+			{
+				Debug.Log ("not initialized");
+				return;
+			}
+			rules.ball.owner.score++;
+		}
+	}
+}
